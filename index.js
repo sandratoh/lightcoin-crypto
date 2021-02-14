87184af80eb5fd06a40eb55635d085afa2bfc0ce
Sandra To <sandra.to.e@gmail.com>
2021-02-14 06:28:03 +0000

    Practice using oop

--- index.js
+++ index.js
@@ -6,14 +6,36 @@ class Transcation {
     this.account = account;
   }
 
+  // isAllowed() {
+  //   if (this.balance > 0 && Withdrawal.amount < this.balance) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
   commit() {
-    this.account.balance += this.value;
+    if (!this.isAllowed()) {
+      return false;
+    } else {
+      // Keep track of the time of the transcation
+      this.time = new Date();
+      // Add the transcation to the account
+      this.account.addTranscation(this);
+      // this.account.balance += this.value; // old code
+      return true;
+    }
   }
 }
 
 class Withdrawal extends Transcation {
   get value() {
     return -this.amount;
+  }
+
+  isAllowed() {
+    // note how it has access to this.account b/c of parents
+    return (this.account.balance - this.amount >= 0);
   }
   // commit() {
   //   this.account.balance -= this.amount;
@@ -25,6 +47,11 @@ class Deposit extends Transcation {
   get value() {
     return this.amount;
   }
+
+  isAllowed() {
+    // deposit is always allowed thanks to capitalism
+    return true;
+  }
   // commit() {
   //   this.account.balance += this.amount;
   // }
@@ -34,7 +61,21 @@ class Account {
   constructor(username) {
     this.username = username;
     // Have the account balance start at $0 since that makes more sense.
-    this.balance = 0;
+    this.transcations = [];
+  }
+
+  get balance() {
+    // Calculate the balance using the transcation objects
+    let balance = 0;
+    for (let t of this.transcations) {
+      console.log(t.value);
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTranscation(transcation) {
+    this.transcations.push(transcation);
   }
 }
 
@@ -43,23 +84,28 @@ class Account {
 
 const myAccount = new Account('snow-patrol');
 
+console.log('Starting Account Balance: ', myAccount.balance);
+
 // Concept: Dependency Injection
 // passing an object the things it needs,
 // rather than having the object access them itself
-let t1 = new Withdrawal(50.25, myAccount);
-t1.value;
-// t1.commit();
-console.log('Transaction 1:', t1);
-
-let t2 = new Withdrawal(9.99, myAccount);
-t2.value;
-// t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Attempting to withdraw $50 should fail...');
+let t1 = new Withdrawal(50, myAccount);
+// t1.value; /// -50.25
+console.log('Commit result: ', t1.commit());
+// console.log('Transaction 1:', t1);
+console.log('Account Balance: ', myAccount.balance);
 
 
-let t3 = new Deposit(120.00, myAccount);
-t3.value;
-// t3.commit();
-console.log('Transcation 3:', t3);
+console.log('Depositing should succeed...');
+let t2 = new Deposit(9.99, myAccount);
+console.log('Commit result: ', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Balance:', myAccount.balance);
+console.log('Withdrawal for $6.99 should be allowed now...');
+let t3 = new Withdrawal(6.99, myAccount);
+console.log('Commit result: ', t3.commit());
+console.log('Account Balance: ', myAccount.balance);
+
+console.log('Ending Account Balance:', myAccount.balance);
+console.log('Looks like I\'m broke again');
